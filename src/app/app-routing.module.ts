267664386794b9f49ebe43d8ce96d2fbd31de42c
Miguel Angel Cornejo Manzanares <miguel.cornejo.manzanares@gmail.com>
2019-09-23 17:58:23 +0200

    Add first lines for comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Ruta Index
+  { path: '', component: HomeComponent },
+  //Ruta Home
+  { path: 'home', component: HomeComponent },
+  //Ruta Error
+  { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
